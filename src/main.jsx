ade52b6dce3ba8d57fa25cc8f4a977f5295814ea
habibmir808 <habibmir808@gmail.com>
2023-03-17 20:09:09 +0600

    add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import {SnackbarProvider}  from 'notistack'
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <Provider store={store}>
-    <SnackbarProvider maxSnack={3}>
-    <App />
-    </SnackbarProvider>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider maxSnack={3}>
+        <App />
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode>
 );
